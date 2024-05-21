2ac61b036ad759c3e86b2f025a29b9107c3b6ad8
Samyar <75088294+samyarkd@users.noreply.github.com>
2024-05-21 20:03:35 +0330

    fix: migrator_script.py -f psql should be written in the begging
    For some reason it works for me this way.

--- mini-app/database-management/migrator_script.py
+++ mini-app/database-management/migrator_script.py
@@ -11,7 +11,7 @@ load_dotenv()
 # Function to execute a migration
 def run_migration(migration_file):
     """Executes a single migration file using the DATABASE_URL"""
-    command = f"psql {DB_URL} -f {os.path.join(MIGRATIONS_PATH, migration_file)}"
+    command = f"psql -f {os.path.join(MIGRATIONS_PATH, migration_file)} {DB_URL}"
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
 
     if result.returncode != 0:
@@ -31,7 +31,7 @@ if __name__ == "__main__":
         if filename.endswith(".sql"):
             run_migration(filename)
 
-    command = f"psql {DB_URL} -f {RESTORE_FILE}"
+    command = f"psql -f {RESTORE_FILE} {DB_URL} "
     subprocess.run(command, shell=True, capture_output=True, text=True)
     
     print("All migrations completed.")
