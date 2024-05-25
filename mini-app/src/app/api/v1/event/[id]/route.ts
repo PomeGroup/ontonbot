68b5fbe69d393c537dce100d42b11ea3eecfb3d0
Samyar <samyarkd@gmail.com>
2024-05-25 19:53:45 +0330

    feat: get event data api

--- mini-app/src/app/api/v1/event/[id]/route.ts
+++ mini-app/src/app/api/v1/event/[id]/route.ts
@@ -1,3 +1,28 @@
-export async function GET(request: Request, res: Response) {
-    return res.json()
+import { db } from '@/db/db'
+import { events } from '@/db/schema'
+import { eq } from 'drizzle-orm'
+
+export async function GET(_: Request, { params }: { params: { id: string } }) {
+    const eventId = parseInt(params.id)
+
+    if (isNaN(eventId)) {
+        return Response.json({ error: 'Invalid event ID' }, { status: 400 })
+    }
+
+    // get event data using drizzle
+    const data = await db
+        .select()
+        .from(events)
+        .where(eq(events.event_id, eventId))
+        .execute()
+
+    // error 400 if not found
+    if (data.length === 0) {
+        return Response.json({ error: 'Event not found' }, { status: 400 })
+    }
+
+    // return event data
+    return Response.json(data[0], {
+        status: 200,
+    })
 }
