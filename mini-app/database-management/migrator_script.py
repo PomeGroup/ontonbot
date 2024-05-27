e8f38b29448594a62891a9157d7d4319af9c02aa
Samyar <samyarkd@gmail.com>
2024-05-27 20:48:44 +0330

    refact: disable data migration from digital ocean

--- mini-app/database-management/migrator_script.py
+++ mini-app/database-management/migrator_script.py
@@ -1,8 +1,8 @@
 import os
 import subprocess
 
+from database_configuration import DB_URL, MIGRATIONS_PATH, RESTORE_FILE
 from dotenv import load_dotenv
-from database_configuration import DB_URL, MIGRATIONS_PATH ,RESTORE_FILE
 
 # Load environment variables (including DATABASE_URL)
 load_dotenv()
@@ -32,7 +32,7 @@ if __name__ == "__main__":
         if filename.endswith(".sql"):
             run_migration(filename)
 
-    command = f"psql -f {RESTORE_FILE} {DB_URL} "
-    subprocess.run(command, shell=True, capture_output=True, text=True)
+    # command = f"psql -f {RESTORE_FILE} {DB_URL} "
+    # subprocess.run(command, shell=True, capture_output=True, text=True)
     
     print("All migrations completed.")
