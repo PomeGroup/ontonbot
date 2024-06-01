0f5fba80300f0456430819eb274935f83800c33b
Samyar <samyarkd@gmail.com>
2024-06-01 12:33:11 +0330

    feat: add ticket and organizer data to get event route

--- mini-app/src/app/api/v1/event/[id]/route.ts
+++ mini-app/src/app/api/v1/event/[id]/route.ts
@@ -1,28 +1,50 @@
 import { db } from '@/db/db'
-import { events } from '@/db/schema'
+import { events, eventTicket, users } from '@/db/schema'
 import { eq } from 'drizzle-orm'
 
 export async function GET(_: Request, { params }: { params: { id: string } }) {
-    const eventId = parseInt(params.id)
-
-    if (isNaN(eventId)) {
-        return Response.json({ error: 'Invalid event ID' }, { status: 400 })
-    }
+    const eventId = params.id
 
     // get event data using drizzle
-    const data = await db
+    const event = await db
         .select()
         .from(events)
-        .where(eq(events.event_id, eventId))
+        .where(eq(events.event_uuid, eventId))
         .execute()
 
     // error 400 if not found
-    if (data.length === 0) {
+    if (event.length === 0) {
         return Response.json({ error: 'Event not found' }, { status: 400 })
     }
 
+    const organizer = (
+        await db
+            .select()
+            .from(users)
+            .where(eq(users.user_id, event[0].owner as number))
+            .execute()
+    )[0]
+
+    const ticket = event[0].ticketToCheckIn
+        ? (
+              await db
+                  .select()
+                  .from(eventTicket)
+                  .where(eq(eventTicket.event_id, event[0].event_id))
+                  .execute()
+          )[0]
+        : undefined
+
+    const data = {
+        ...event[0],
+        ticket,
+        organizer,
+    }
+
     // return event data
-    return Response.json(data[0], {
+    return Response.json(data, {
         status: 200,
     })
 }
+
+export const dynamic = 'force-dynamic'
