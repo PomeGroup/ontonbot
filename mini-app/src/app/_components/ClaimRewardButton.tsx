bede863783e35f837836f1c1ce152f4aeef4d3f9
Samyar <samyarkd@gmail.com>
2024-07-13 20:12:52 +0330

    fix: reward link

--- mini-app/src/app/_components/ClaimRewardButton.tsx
+++ mini-app/src/app/_components/ClaimRewardButton.tsx
@@ -5,39 +5,47 @@ import { trpc } from "../_trpc/client";
 import { useEffect, useState } from "react";
 import { useMainButton, useUtils } from "@tma.js/sdk-react";
 
-export function ClaimRewardButton(props: { eventId: string }) {
-    const WebApp = useWebApp()
-    const initData = WebApp?.initData || ''
-    const visitorReward = trpc.users.getVisitorReward.useQuery({ init_data: initData, event_uuid: props.eventId })
+// Child component
+function ClaimRewardButtonChild({ link }: { link: string }) {
     const mainButton = useMainButton(true)
     const tmaUtils = useUtils(true)
-    const [rewardLink, setRewardLink] = useState<string | undefined>(undefined)
-
-    useEffect(() => {
-        if (visitorReward.isSuccess && visitorReward.data?.data) {
-            setRewardLink(visitorReward.data.data as string)
-        }
-    }, [visitorReward.isSuccess, visitorReward.data?.data])
 
     function openRewardLink() {
-        console.log({ rewardLink })
-        if (rewardLink) {
-            tmaUtils?.openLink(rewardLink)
-        }
+        console.log({ link })
+        tmaUtils?.openLink(link)
     }
 
     useEffect(() => {
-        if (rewardLink) {
-            mainButton?.setText("Claim Reward")
-            mainButton?.on('click', openRewardLink)
-            mainButton?.enable().show()
-        }
+        mainButton?.setText("Claim Reward")
+        mainButton?.on('click', openRewardLink)
+        mainButton?.enable().show()
 
         return () => {
             mainButton?.off('click', openRewardLink)
             mainButton?.hide().disable()
         }
-    }, [rewardLink, mainButton])
+    }, [mainButton])
 
     return null
+}
+
+// Parent component
+export function ClaimRewardButton(props: { eventId: string }) {
+    const WebApp = useWebApp()
+    const initData = WebApp?.initData || ''
+    const visitorReward = trpc.users.getVisitorReward.useQuery({ init_data: initData, event_uuid: props.eventId })
+    const [rewardLink, setRewardLink] = useState<string | undefined>(undefined)
+
+    useEffect(() => {
+        if (visitorReward.isSuccess && visitorReward.data?.data) {
+            console.log({ rewardLink: visitorReward.data })
+            setRewardLink(visitorReward.data.data as string)
+        }
+    }, [visitorReward.isSuccess, visitorReward.data?.data])
+
+    if (!rewardLink) {
+        return null
+    }
+
+    return <ClaimRewardButtonChild link={rewardLink} />
 }
