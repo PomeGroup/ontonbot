15acf752a6e48d78f304aae95f14080613579be0
samyar <samyarkd@gmail.com>
2024-07-29 12:35:33 +0330

    feat: migrations for reweard changes

---
+++ mini-app/drizzle/0005_odd_viper.sql
@@ -0,0 +1,8 @@
+DO $$ BEGIN
+ CREATE TYPE "reward_status" AS ENUM('pending_creation', 'created', 'received', 'notified', 'failed');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+ALTER TABLE "rewards" ADD COLUMN "try_count" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
+ALTER TABLE "rewards" ADD COLUMN "status" "reward_status" DEFAULT 'created' NOT NULL;
