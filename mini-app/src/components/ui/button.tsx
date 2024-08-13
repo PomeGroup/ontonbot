86af7eb1f1f68805ab00de926301e9146a6fa599
samyar <samyarkd@gmail.com>
2024-08-13 10:31:15 +0330

    style: button style

--- mini-app/src/components/ui/button.tsx
+++ mini-app/src/components/ui/button.tsx
@@ -13,8 +13,7 @@ const buttonVariants = cva(
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input bg-separator hover:bg-accent hover:text-accent-foreground",
+        outline: "bg-primary/10 text-secondary-foreground hover:bg-primary/20",
         secondary:
           "bg-primary/10 text-secondary-foreground hover:bg-primary/20",
         ghost: "hover:bg-accent hover:text-accent-foreground",
