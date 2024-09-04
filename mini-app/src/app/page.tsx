b747211ae638cdd90a8a6644c820556085a2fd6e
radiophp <radiophp@gmail.com>
2024-09-04 21:32:44 +0300

    fix: home page Re-rendering + create event button blinking + add swiper tab - issues : #141 #142 #142

--- mini-app/src/app/page.tsx
+++ mini-app/src/app/page.tsx
@@ -1,314 +1,251 @@
 "use client";
-import React, {  useEffect, useState } from "react";
+import React, {  useCallback, useEffect, useRef, useState } from "react";
 import EventCard from "@/app/_components/EventCard/EventCard";
 import EventCardSkeleton from "@/app/_components/EventCard/EventCardSkeleton";
 import SearchBar from "@/app/_components/SearchBar/SearchBar";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import useAuth from "@/hooks/useAuth";
 import useWebApp from "@/hooks/useWebApp";
 import searchEventsInputZod from "@/zodSchema/searchEventsInputZod";
 import { useRouter } from "next/navigation";
-import MainButton from "./_components/atoms/buttons/web-app/MainButton";
 import { trpc } from "./_trpc/client";
 import "./page.css";
-import zod from "zod";
 import { useConfig } from "@/context/ConfigContext";
 import Image from "next/image";
-// Memoized MainButton to prevent unnecessary re-renders
+import MemoizedMainButton from "@/app/_components/Memoized/MemoizedMainButton";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+// Define types for events
+type EventData = any[];
+
+
 
 export default function Home() {
-
-  const { config  } = useConfig();
+  const { config } = useConfig();
   const SliderEventUUID = config?.homeSliderEventUUID || "";
-  console.log("SliderEventUUID", SliderEventUUID);
   const webApp = useWebApp();
-  const {
-    authorized,
-    isLoading: useAuthLoading,
-    role: userRole,
-  } = useAuth();
-  const UserId = webApp?.initDataUnsafe?.user?.id ;
-
+  const { authorized, isLoading: useAuthLoading, role: userRole } = useAuth();
+  const UserId = webApp?.initDataUnsafe?.user?.id;
 
   const router = useRouter();
-  const [isMyEventsTabActive, setIsMyEventsTabActive] = useState(false);
-  const createSearchQueryParams = (
-    params: zod.infer<typeof searchEventsInputZod>
-  ) => {
-    return new URLSearchParams(
-      Object.entries({
-        query: params.search || "",
-        startDate: params.filter?.startDate?.toString() || "",
-        startDateOperator: params.filter?.startDateOperator || "",
-        endDate: params.filter?.endDate?.toString() || "",
-        endDateOperator: params.filter?.endDateOperator || "",
-        sortBy: params.sortBy || "default",
-      }).filter(([_, value]) => value !== "")
-    ).toString();
-  };
+  const [activeTab, setActiveTab] = useState("all-events");
+  const swiperRef = useRef<any>(null);
 
+  // Initialize state with empty arrays to avoid undefined errors
+  const [sliderEventData, setSliderEventData] = useState<EventData>([]);
+  const [upcomingEventsData, setUpcomingEventsData] = useState<EventData>([]);
+  const [pastEventsData, setPastEventsData] = useState<EventData>([]);
+  const [myEventsData, setMyEventsData] = useState<EventData>([]);
 
-  const upcomingEventsParams = searchEventsInputZod.parse({
-    limit: 2,
-    offset: 0,
-    filter: {
-      participationType: ["online", "in_person"],
-      startDate:
-        Math.floor(Date.now() / 1000) - (Math.floor(Date.now() / 1000) % 600),
-    },
-    sortBy: "start_date_asc",
-  });
-  const ongoingEventsParams = searchEventsInputZod.parse({
-    limit: 2,
-    offset: 0,
-    filter: {
-      participationType: ["online", "in_person"],
-      startDate: Math.floor(Date.now() / 1000),
-      startDateOperator: "<=",
-      endDate: Math.floor(Date.now() / 1000),
-      endDateOperator: ">=",
-    },
-    sortBy: "start_date_asc",
-  });
-  const pastEventsParams = searchEventsInputZod.parse({
-    limit: 2,
-    offset: 0,
-    filter: {
-      participationType: ["online", "in_person"],
-      endDate:
-        Math.floor(Date.now() / 1000) - (Math.floor(Date.now() / 1000) % 600),
-
-    },
-    sortBy: "start_date_desc",
-  });
-
+  // Fetch parameters
   const sliderEventParams = searchEventsInputZod.parse({
     limit: 1,
     filter: {
       event_uuids: [SliderEventUUID],
     },
   });
-  const seeAllUpcomingEventsLink =
-    "/search/?" + createSearchQueryParams(upcomingEventsParams);
-  const seeAllPastEventsLink =
-    "/search/?" + createSearchQueryParams(pastEventsParams);
+
+  const upcomingEventsParams = searchEventsInputZod.parse({
+    limit: 2,
+    offset: 0,
+    filter: {
+      participationType: ["online", "in_person"],
+      startDate: Math.floor(Date.now() / 1000),
+    },
+    sortBy: "start_date_asc",
+  });
+
+  const pastEventsParams = searchEventsInputZod.parse({
+    limit: 2,
+    offset: 0,
+    filter: {
+      participationType: ["online", "in_person"],
+      endDate: Math.floor(Date.now() / 1000) - (Math.floor(Date.now() / 1000) % 600),
+    },
+    sortBy: "start_date_desc",
+  });
 
   const myEventsParams = searchEventsInputZod.parse({
     limit: 0,
     offset: 0,
     filter: {
-      // organizer_user_id: UserId,
       user_id: UserId,
     },
     sortBy: "start_date_desc",
   });
 
-  const {
-    data: upcomingEvents,
-    isLoading: isLoadingUpcoming,
-    isError: isErrorUpcoming,
-  } = trpc.events.getEventsWithFilters.useQuery(upcomingEventsParams);
+  // Queries with `enabled: false` to prevent automatic fetching
+  const { refetch: refetchSliderEvent } = trpc.events.getEventsWithFilters.useQuery(sliderEventParams, {
+    enabled: false,
+  });
 
-  const {
-    data: ongoingEvents,
-    isLoading: isLoadingOngoing,
-    isError: isErrorOngoing,
-  } = trpc.events.getEventsWithFilters.useQuery(ongoingEventsParams);
-  useEffect(() => {
-    console.log("ongoingEvents", ongoingEvents);
-  }, [ongoingEvents]);
-  const {
-    data: pastEvents,
-    isLoading: isLoadingPast,
-    isError: isErrorPast,
-  } = trpc.events.getEventsWithFilters.useQuery(pastEventsParams);
+  const { refetch: refetchUpcomingEvents } = trpc.events.getEventsWithFilters.useQuery(upcomingEventsParams, {
+    enabled: false,
+  });
 
-  const {
-    data: sliderEvent,
-    isLoading: isLoadingSlider,
-    isError: isErrorSlider,
-  } = trpc.events.getEventsWithFilters.useQuery(sliderEventParams);
+  const { refetch: refetchPastEvents } = trpc.events.getEventsWithFilters.useQuery(pastEventsParams, {
+    enabled: false,
+  });
 
-  const {
-    data: myEvents,
-    isLoading: isLoadingMyEvents,
-    isError: isErrorMyEvents,
-    refetch: refetchMyEvents,
-  } = trpc.events.getEventsWithFilters.useQuery(myEventsParams, {
-    enabled: false, // Disable automatic fetching
+  const { refetch: refetchMyEvents } = trpc.events.getEventsWithFilters.useQuery(myEventsParams, {
+    enabled: false,
   });
 
   useEffect(() => {
-    if (isMyEventsTabActive) {
-      refetchMyEvents().then(() =>   console.log("----use myEvents", myEvents));
-    }
-  }, [isMyEventsTabActive, refetchMyEvents]);
-
-
-  const error = isErrorUpcoming || isErrorPast || isErrorSlider || isErrorMyEvents || isErrorOngoing;
-  const isLoading = isLoadingUpcoming || isLoadingPast || isLoadingSlider  || isLoadingOngoing ;
-  if(error) {
-    console.error("Error fetching data", {
-      isErrorUpcoming,
-      isErrorPast,
-      isErrorSlider,
-      isErrorMyEvents,
-
+    // Fetch Slider Events
+    refetchSliderEvent().then((res) => {
+      if (res.status === "success" && Array.isArray(res.data?.data)) {
+        setSliderEventData(res.data.data);
+      }
     });
-    return  (
-        <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">
-          An error occurred while fetching data. Please try again later.
-        </div>
-    )
-  }
+
+    // Fetch Upcoming Events
+    refetchUpcomingEvents().then((res) => {
+      if (res.status === "success" && Array.isArray(res.data?.data)) {
+        setUpcomingEventsData(res.data.data);
+      }
+    });
+
+    // Fetch Past Events
+    refetchPastEvents().then((res) => {
+      if (res.status === "success" && Array.isArray(res.data?.data)) {
+        setPastEventsData(res.data.data);
+      }
+    });
+  }, [refetchSliderEvent, refetchUpcomingEvents, refetchPastEvents]);
+
+  // Handle tab click and data fetching logic
+  const handleTabClick = (value: string) => {
+    setActiveTab(value);
+    const slideIndex = value === "all-events" ? 0 : 1;
+    swiperRef.current?.slideTo(slideIndex);
+
+    // Fetch data when switching tabs
+    if (value === "my-events" && myEventsData.length === 0) {
+      refetchMyEvents().then((res) => {
+        if (Array.isArray(res.data?.data)) setMyEventsData(res.data.data);
+      });
+    }
+  };
+
+  // Handle swiper slide change
+  const handleSlideChange = (swiper: any) => {
+    const activeIndex = swiper.activeIndex;
+    const newTab = activeIndex === 0 ? "all-events" : "my-events";
+    setActiveTab(newTab);
+
+    if (newTab === "my-events" && myEventsData.length === 0) {
+      refetchMyEvents().then((res) => {
+        if (Array.isArray(res.data?.data)) setMyEventsData(res.data.data);
+      });
+    }
+  };
+
+  // Memoized handler for the "Create Event" button
+  const handleCreateEvent = useCallback(() => {
+    router.push("/events/create");
+  }, [router]);
+
   return (
-    <>
-      {error && (
-        <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">
-          An error occurred while fetching data. Please try again later.
-        </div>
-      )}
-      <SearchBar
-        includeQueryParam={false}
-        onUpdateResults={() => {}}
-      />
+      <>
+        <SearchBar includeQueryParam={false} onUpdateResults={() => {}} />
 
-      <Tabs
-        defaultValue="all-events"
-        className="pt-2"
-        onValueChange={(value) => setIsMyEventsTabActive(value === "my-events")}
-      >
-        <TabsList className="flex bg-gray-600 h-33 rounded-lg p-1">
-          <TabsTrigger
-            value="all-events"
-            className="flex-1 p-2 rounded-lg text-center font-medium text-white focus:outline-none focus:ring-0 ring-offset-0 transition ease-in-out duration-150"
-          >
-            All events
-          </TabsTrigger>
-          <TabsTrigger
-            value="my-events"
-            className="flex-1 p-2 rounded-lg text-center font-medium text-white focus:outline-none focus:ring-0 ring-offset-0 transition ease-in-out duration-150"
-          >
-            My events
-          </TabsTrigger>
-        </TabsList>
-
-        <TabsContent value="all-events">
-          <div className="pt-2 w-full">
-            <>
-
-              {isLoading ? (
-                <EventCardSkeleton mode={"detailed"} />
-              ) : (
-                <>
-                  {sliderEvent?.data?.length &&
-                    sliderEvent?.data?.length > 0 && (
-                      <EventCard
-                        event={sliderEvent?.data[0]}
-                        mode={"detailed"}
-                        currentUserId={UserId}
-                      />
-                    )}
-                </>
-              )}
-            </>
-          </div>
-
-          {ongoingEvents?.data && ongoingEvents?.data?.length > 0 && (
-              <>
-              <div className="pt-4  w-full pb-4  flex justify-between items-center">
-                <h2 className="font-bold text-lg">Ongoing Events</h2>
-                {/* Optionally, add a link to see all ongoing events */}
-              </div>
-              {isLoading
-                  ? [1, 2].map((index) => <EventCardSkeleton key={index} />)
-                  : ongoingEvents?.data?.map((event) => (
-                      <EventCard
-                          key={event.event_uuid}
-                          event={event}
-                          mode={"ongoing"}
-                          currentUserId={UserId}
-                      />
-                  ))}
-              </>
-        )}
-
-          <div className="pt-4  w-full pb-4  flex justify-between items-center">
-            <h2 className="font-bold text-lg">Upcoming Events</h2>
-            <a
-              href={seeAllUpcomingEventsLink}
-              className="text-zinc-300 hover:underline"
+        <Tabs value={activeTab} className="pt-2" onValueChange={handleTabClick}>
+          <TabsList className="flex bg-gray-600 h-33 rounded-lg p-1">
+            <TabsTrigger
+                value="all-events"
+                className={`flex-1 p-2 rounded-lg text-center font-medium text-white focus:outline-none ${
+                    activeTab === "all-events" ? "bg-blue-600" : "bg-transparent"
+                }`}
             >
-              See All
-            </a>
-          </div>
-          {isLoading
-            ? [1, 2].map((index) => <EventCardSkeleton key={index} />)
-            : upcomingEvents?.data?.map((event) => (
-                <EventCard
-                  key={event.event_uuid}
-                  event={event}
-                  currentUserId={UserId}
-                />
-              ))}
-          <div className="pt-4 pb-4 flex justify-between items-center">
-            <h2 className="font-bold text-lg">Past Events</h2>
-            <a
-              href={seeAllPastEventsLink}
-              className="text-zinc-300  hover:underline"
+                All events
+            </TabsTrigger>
+            <TabsTrigger
+                value="my-events"
+                className={`flex-1 p-2 rounded-lg text-center font-medium text-white focus:outline-none ${
+                    activeTab === "my-events" ? "bg-blue-600" : "bg-transparent"
+                }`}
             >
-              See All
-            </a>
-          </div>
-          {isLoading
-            ? [1, 2].map((index) => <EventCardSkeleton key={index} />)
-            : pastEvents?.data?.map((event) => (
-                <EventCard
-                  key={event.event_uuid}
-                  event={event}
-                  currentUserId={UserId}
-                />
-              ))}
-        </TabsContent>
+              My events
+            </TabsTrigger>
+          </TabsList>
 
-        <TabsContent value="my-events">
-          <div className="pt-2">
-            {isLoadingMyEvents ? (
-              [1, 2].map((index) => <EventCardSkeleton key={index} />)
-            ) : (myEvents?.data?.length === 0  ) ? (
-              <div className="flex flex-col items-center justify-center min-h-screen text-center space-y-4">
-                <div>
-                  <Image
-                    src={"/template-images/my-event-empty-list-msg.png"}
-                    alt={"There are no Events"}
-                    width={180}
-                    height={180}
-                  />
+          <Swiper
+              onSlideChange={handleSlideChange}
+              slidesPerView={1}
+              spaceBetween={30}
+              pagination={{ clickable: true }}
+              onSwiper={(swiper) => {
+                swiperRef.current = swiper;
+              }}
+          >
+            {/* All Events Slide */}
+            <SwiperSlide>
+              <div className="pt-2">
+
+
+                {sliderEventData.length === 0 ? (
+                    <EventCardSkeleton mode={"detailed"} />
+                ) : (
+                    <EventCard event={sliderEventData[0]} mode={"detailed"} currentUserId={UserId} />
+                )}
+
+                <div className="pt-4 w-full pb-4 flex justify-between items-center">
+                  <h2 className="font-bold text-lg">Upcoming Events</h2>
+                  <a href="/search/?type=upcoming" className="text-zinc-300 hover:underline">
+                    See All
+                  </a>
                 </div>
-                <div className="text-gray-500 max-w-md">
-                  There are no Events <br/>at the time.
+
+                {upcomingEventsData.length === 0 ? (
+                    <EventCardSkeleton />
+                ) : (
+                    upcomingEventsData.map((event) => (
+                        <EventCard key={event.event_uuid} event={event} currentUserId={UserId} />
+                    ))
+                )}
+
+                <div className="pt-4 pb-4 flex justify-between items-center">
+                  <h2 className="font-bold text-lg">Past Events</h2>
+                  <a href="/search/?type=past" className="text-zinc-300 hover:underline">
+                    See All
+                  </a>
                 </div>
+
+                {pastEventsData.length === 0 ? (
+                    <EventCardSkeleton />
+                ) : (
+                    pastEventsData.map((event) => (
+                        <EventCard key={event.event_uuid} event={event} currentUserId={UserId} />
+                    ))
+                )}
               </div>
-            ) : (
-              myEvents?.data?.map((event) => (
-                <EventCard
-                  key={event.event_uuid}
-                  event={event}
-                  currentUserId={UserId}
-                />
-              ))
-            )}
-          </div>
-        </TabsContent>
-      </Tabs>
-      {!useAuthLoading &&
-        (userRole === "admin" || userRole === "organizer") &&
-        authorized && (
-          <MainButton
-            text="Create new event"
-            onClick={() => router.push("/events/create")}
-          />
-        )}
-    </>
+            </SwiperSlide>
+
+            {/* My Events Slide */}
+            <SwiperSlide>
+              <div className="pt-2">
+                {myEventsData.length === 0 ? (
+                    [1, 2].map((index) => <EventCardSkeleton key={index} />)
+                ) : myEventsData.length === 0 ? (
+                    <div className="flex flex-col items-center justify-center min-h-screen text-center space-y-4">
+                      <Image src={"/template-images/my-event-empty-list-msg.png"} alt={"No Events"} width={180} height={180} />
+                      <div className="text-gray-500 max-w-md">No Events at this time.</div>
+                    </div>
+                ) : (
+                    myEventsData.map((event) => (
+                        <EventCard key={event.event_uuid} event={event} currentUserId={UserId} />
+                    ))
+                )}
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </Tabs>
+
+        {!useAuthLoading &&
+            (userRole === "admin" || userRole === "organizer") &&
+            authorized && <MemoizedMainButton text="Create new event" onClick={handleCreateEvent} />}
+      </>
   );
 }
