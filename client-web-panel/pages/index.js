365db69c96aae6f9c82fab4af984fd90b081a22d
radiophp <radiophp@gmail.com>
2024-10-09 17:48:13 +0300

    fix: testing problem of env var

--- client-web-panel/pages/index.js
+++ client-web-panel/pages/index.js
@@ -4,7 +4,8 @@ import SignInForm from "@/components/Authentication/SignInForm";
 
 
 const Index = () => {
-
+console.log(process.env.NEXT_PUBLIC_BACKEND_URL_CLIENT);
+console.log(process.env)
   return (
     <>
       <SignInForm />
