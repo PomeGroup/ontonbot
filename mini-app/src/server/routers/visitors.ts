b54434b340b7a6bb1a20f1fef85d365dd799cfc4
samyar <samyarkd@gmail.com>
2024-10-09 19:08:00 +0330

    hotfix: add visitor 1.5.2

--- mini-app/src/server/routers/visitors.ts
+++ mini-app/src/server/routers/visitors.ts
@@ -1,14 +1,20 @@
-import { validateMiniAppData } from "@/utils";
+import { db } from "@/db/db";
+import { visitors } from "@/db/schema";
+import { TRPCError } from "@trpc/server";
+import { and, eq } from "drizzle-orm";
 import { z } from "zod";
 import { selectVisitorsByEventUuid } from "../db/visitors";
-import { eventManagementProtectedProcedure, publicProcedure, router} from "../trpc";
+import {
+  eventManagementProtectedProcedure,
+  initDataProtectedProcedure,
+  router,
+} from "../trpc";
 
 export const visitorsRouter = router({
   // protect
   getAll: eventManagementProtectedProcedure
     .input(
       z.object({
-
         limit: z.number().optional(),
         cursor: z.number().optional(),
         dynamic_fields: z.boolean().optional(),
@@ -16,29 +22,55 @@ export const visitorsRouter = router({
       })
     )
     .query(async (opts) => {
-      const {  limit = 25, cursor = 0 ,dynamic_fields =true} = opts.input;
-      const data = await selectVisitorsByEventUuid(opts.input.event_uuid, limit, cursor ,dynamic_fields ,opts.input.search);
-      return { ...data, event : opts.ctx.event || null };
+      const { limit = 25, cursor = 0, dynamic_fields = true } = opts.input;
+      const data = await selectVisitorsByEventUuid(
+        opts.input.event_uuid,
+        limit,
+        cursor,
+        dynamic_fields,
+        opts.input.search
+      );
+      return { ...data, event: opts.ctx.event || null };
     }),
 
   // protect
-  add: publicProcedure
+  add: initDataProtectedProcedure
     .input(
       z.object({
-        initData: z.string().optional(),
         event_uuid: z.string(),
       })
     )
     .mutation(async (opts) => {
-      if (!opts.input.initData) {
-        return;
+      const existingVisitor = await db
+        .select()
+        .from(visitors)
+        .where(
+          and(
+            eq(visitors.user_id, opts.ctx.parsedInitData.user.id),
+            eq(visitors.event_uuid, opts.input.event_uuid)
+          )
+        )
+        .execute();
+
+      if (existingVisitor.length !== 0) {
+        throw new TRPCError({
+          code: "CONFLICT",
+          message: "Visitor already exists for this event",
+        });
       }
 
-      const { valid } = validateMiniAppData(opts.input.initData);
+      await db
+        .insert(visitors)
+        .values({
+          user_id: opts.ctx.parsedInitData.user.id,
+          event_uuid: opts.input.event_uuid,
+          updatedBy: opts.ctx.parsedInitData.user.id.toString(),
+          updatedAt: new Date(),
+        })
+        .execute();
 
-      if (!valid) {
-        return;
-      }
-
+      return {
+        message: "visitor added",
+      };
     }),
 });
