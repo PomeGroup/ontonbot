c4ea0184ae1e0ac6956b5387029e644244970fe1
radiophp <radiophp@gmail.com>
2024-10-12 09:48:21 +0300

    env example

--- mini-app/src/app/_trpc/Provider.tsx
+++ mini-app/src/app/_trpc/Provider.tsx
@@ -8,23 +8,18 @@ import React, { useState } from "react";
 
 import { trpc } from "./client";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3, // Retry failed queries 2 times
+      retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 5000), // Exponential backoff
+      cacheTime: 60 * 1000, // Cache data for 5 minutes
+      staleTime: 2 * 60 * 1000, // Consider data stale after 2 minutes
+      refetchOnWindowFocus: false, // Disable refetch on window focus
+    },
+  },
+});
 export default function Provider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(
-      () =>
-          new QueryClient({
-            defaultOptions: {
-              queries: {
-                retry: 3, // Retry failed queries 2 times
-                retryDelay: (attemptIndex) =>
-                    Math.min(1000 * 2 ** attemptIndex, 5000), // Exponential backoff
-                cacheTime: 60 * 1000, // Cache data for 5 minutes
-                staleTime: 2 * 60 * 1000, // Consider data stale after 2 minutes
-                refetchOnWindowFocus: false, // Disable refetch on window focus
-              },
-            },
-          })
-  );
-
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
