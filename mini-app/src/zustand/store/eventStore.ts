1d340582876ea81c7c62d847079a120d1570633c
saremeskandary <saremeskandary@gmail.com>
2024-10-14 15:43:35 +0330

    feat: ticketAttributes added to store

--- mini-app/src/zustand/store/eventStore.ts
+++ mini-app/src/zustand/store/eventStore.ts
@@ -2,14 +2,25 @@
 import { create } from "zustand";
 import { ReactNode } from "react";
 
+// Define EventAttributes type
 type EventAttributes = [string, ReactNode][];
+type TicketAttributes = [string, ReactNode][]; // New type for ticket attributes
 
+// Extend the EventStoreState interface to include ticketAttributes
 interface EventStoreState {
   attributes: EventAttributes;
-  setAttributes: (newAttributes: EventAttributes) => void;
+  setAttributes: (_newAttributes: EventAttributes) => void;
+  ticketAttributes: TicketAttributes; // New ticketAttributes property
+  setTicketAttributes: (_newTicketAttributes: TicketAttributes) => void; // New setter for ticketAttributes
 }
 
+// Create Zustand store with event and ticket attributes
 export const useEventStore = create<EventStoreState>((set) => ({
-  attributes: [],
-  setAttributes: (newAttributes) => set(() => ({ attributes: newAttributes })),
+  attributes: [], // Initial state for event attributes
+  setAttributes: (newAttributes: EventAttributes) =>
+    set(() => ({ attributes: newAttributes })),
+
+  ticketAttributes: [], // Initial state for ticket attributes
+  setTicketAttributes: (newTicketAttributes: TicketAttributes) =>
+    set(() => ({ ticketAttributes: newTicketAttributes })),
 }));
