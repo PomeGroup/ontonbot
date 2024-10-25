dfd1a12e79a01ec3aeae2de610b4b8be4cac7ada
alirezaprf <alirezaadvertise@gmail.com>
2024-10-25 03:33:54 +0330

    fix shared secret

--- mini-app/src/app/api/v1/order/ton-proof/generate-payload/route.ts
+++ mini-app/src/app/api/v1/order/ton-proof/generate-payload/route.ts
@@ -1,10 +1,9 @@
 // app/api/ton-auth/generate-payload/route.ts
 import { NextResponse } from "next/server";
 import crypto from "crypto";
-
+import {SHARED_SECRET} from "@/constants"
 // Ensure you have these constants properly set up in a constants file or environment variables
 const PAYLOAD_TTL = parseInt(process.env.PAYLOAD_TTL || "300", 10); // Time-to-live in seconds
-const SHARED_SECRET = process.env.SHARED_SECRET || "default_ton_jwt_secret";
 
 export async function GET() {
   try {
