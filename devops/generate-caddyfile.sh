1e60d961d0314705a17b8eb06fef6912d65f4c42
radiophp <radiophp@gmail.com>
2024-11-11 20:54:53 +0300

    fix: add cloudlfare dns

--- devops/generate-caddyfile.sh
+++ devops/generate-caddyfile.sh
@@ -1,98 +1,75 @@
 #!/bin/sh
+# Define TLS configuration based on the USE_CLOUDFLARE variable
+echo "Generating Caddyfile... ${USE_CLOUDFLARE}"
+if [ -n "${USE_CLOUDFLARE}" ]; then
+    TLS_CONFIG="tls {
+        dns cloudflare ${CLOUDFLARE_API_TOKEN}
+        protocols tls1.2 tls1.3
+    }"
+else
+    TLS_CONFIG="tls /certs/fullchain.pem /certs/privkey.pem {
+        protocols tls1.2 tls1.3
+    }"
+fi
+
+# Define log configuration
+LOG_CONFIG="log {
+    output stdout
+    format json
+}"
+
+# Generate Caddyfile
 echo "
 
 ${MINI_APP_DOMAIN} {
-    tls /certs/fullchain.pem /certs/privkey.pem {
-        protocols tls1.2 tls1.3
-    }
-    log {
-        output stdout
-        format json
-    }
+    ${TLS_CONFIG}
+    ${LOG_CONFIG}
     reverse_proxy /ptma* http://${IP_RANGE_BASE}:${PARTICIPANT_TMA_PORT}
-    reverse_proxy  http://${IP_RANGE_BASE}:${MINI_APP_PORT}
+    reverse_proxy http://${IP_RANGE_BASE}:${MINI_APP_PORT}
 }
 
 ${METABASE_DOMAIN} {
-    tls /certs/fullchain.pem /certs/privkey.pem {
-        protocols tls1.2 tls1.3
-    }
-    log {
-        output stdout
-        format json
-    }
+    ${TLS_CONFIG}
+    ${LOG_CONFIG}
     reverse_proxy ${IP_RANGE_BASE}:${PORT_METABASE}
 }
 
 ${MINIO_STORAGE_DOMAIN} {
-    tls /certs/fullchain.pem /certs/privkey.pem {
-        protocols tls1.2 tls1.3
-    }
-    log {
-        output stdout
-        format json
-    }
+    ${TLS_CONFIG}
+    ${LOG_CONFIG}
     reverse_proxy http://${IP_MINIO}:${MINIO_PORT}
 }
 
 ${MINIO_STORAGE_ADMIN_DOMAIN} {
-    tls /certs/fullchain.pem /certs/privkey.pem {
-        protocols tls1.2 tls1.3
-    }
-    log {
-        output stdout
-        format json
-    }
+    ${TLS_CONFIG}
+    ${LOG_CONFIG}
     reverse_proxy http://${IP_RANGE_BASE}:${MINIO_DASHBOARD_PORT}
 }
 
 ${MONITORING_DOMAIN} {
-    tls /certs/fullchain.pem /certs/privkey.pem {
-        protocols tls1.2 tls1.3
-    }
-    log {
-        output stdout
-        format json
-    }
+    ${TLS_CONFIG}
+    ${LOG_CONFIG}
     reverse_proxy grafana:3000
 }
 
 ${PGADMIN_DOMAIN} {
-    tls /certs/fullchain.pem /certs/privkey.pem {
-        protocols tls1.2 tls1.3
-    }
-    log {
-        output stdout
-        format json
-    }
+    ${TLS_CONFIG}
+    ${LOG_CONFIG}
     reverse_proxy http://${IP_PGADMIN}:${PORT_PGADMIN}
 }
 
 ${CLIENT_WEB_DOMAIN} {
-    tls /certs/fullchain.pem /certs/privkey.pem {
-        protocols tls1.2 tls1.3
-    }
-    log {
-        output stdout
-        format json
-    }
+    ${TLS_CONFIG}
+    ${LOG_CONFIG}
     reverse_proxy ${IP_RANGE_BASE}:${PORT_CLIENT_WEB}
 }
 
 ${ONTON_DOMAIN} {
-    tls /certs/fullchain.pem /certs/privkey.pem {
-        protocols tls1.2 tls1.3
-    }
-    log {
-        output stdout
-        format json
-    }
+    ${TLS_CONFIG}
+    ${LOG_CONFIG}
     reverse_proxy ${IP_RANGE_BASE}:${PORT_WEB_SITE}
 }
 
-
-
-
 " > /etc/caddy/Caddyfile
-
+cat /etc/caddy/Caddyfile
 exec caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
