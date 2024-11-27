692caf96e7d53809132417b6aa687e2e9010c6c2
radiophp <radiophp@gmail.com>
2024-11-27 19:23:44 +0300

    fix: add commit sha detail to ping api

--- mini-app/src/app/api/client/v1/public/ping/route.ts
+++ mini-app/src/app/api/client/v1/public/ping/route.ts
@@ -1,9 +1,31 @@
 import { NextResponse } from "next/server";
+import { readFile } from "fs/promises";
+import path from "path";
 
 export async function GET() {
   try {
     const uptime = process.uptime();
     const timestamp = new Date().toISOString();
+
+    // Define the path relative to the project's root directory
+    const filePath = path.resolve(process.cwd(), "commit_details.txt");
+
+    // Ensure the file is located within the expected directory (optional additional check)
+    const allowedDir = process.cwd();
+    if (!filePath.startsWith(allowedDir)) {
+      return NextResponse.json(
+            {
+                error: "Access to the specified file is not allowed",
+            },
+            { status: 403 }
+        );
+    }
+
+    // Read the file content
+    const fileContent = await readFile(filePath, "utf-8");
+
+    // Replace \n with ||
+    const commitDetail = fileContent.replace(/\n/g, "   ||  ");
 
     return NextResponse.json({
       success: true,
@@ -12,15 +34,17 @@ export async function GET() {
         uptime: `${Math.floor(uptime)} seconds`,
         timestamp,
       },
+      sha: commitDetail
     });
   } catch (error) {
     return NextResponse.json(
-      {
-        error: (error as Error).message || "An internal server error occurred",
-      },
-      { status: 500 }
+        {
+          error: "An internal server error occurred", // Generic error message
+        },
+        { status: 500 }
     );
   }
 }
+
 
 export const dynamic = "force-dynamic";
