01c5942f231f05988bf58a5d7c32a0246b69653b
samyar <samyarkd@gmail.com>
2024-11-29 17:47:36 +0330

    fix: up

--- mini-app/src/app/_components/KonstaAppProvider.tsx
+++ mini-app/src/app/_components/KonstaAppProvider.tsx
@@ -1,13 +1,19 @@
 "use client";
 
+import useWebApp from "@/hooks/useWebApp";
 import { App } from "konsta/react";
 import React, { ReactNode } from "react";
 
 const KonstaAppProvider = ({ children }: { children: ReactNode }) => {
+  const webApp = useWebApp();
+
   return (
     <App
       theme={"ios"}
       safeAreas={false}
+      style={{
+        minHeight: webApp?.viewportStableHeight,
+      }}
     >
       {children}
     </App>
