b230fa412e783b18ad160c9a96ac5782443b0e91
samyar <samyarkd@gmail.com>
2024-12-03 11:13:40 +0330

    hotfix: change env for nft manager

--- newton/apps/nft-manager/src/main.ts
+++ newton/apps/nft-manager/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log("Onton Api Key", process.env.ONTON_API_KEY)
-  console.log("PTMA base", process.env.PTMA_API_BASE_URL)
+  console.log("PTMA base", process.env.NEXT_PUBLIC_API_BASE_URL)
   // app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.NFT_MANAGER_PORT );
 }
