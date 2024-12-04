04f93bb0c7dfba5554d96a67e5f4ff54ee86c99b
radiophp <radiophp@gmail.com>
2024-12-04 20:25:55 +0300

    fix: header down

--- devops/generate-caddyfile-by-address.sh
+++ devops/generate-caddyfile-by-address.sh
@@ -105,7 +105,7 @@ ${ONTON_DOMAIN} {
 
             reverse_proxy /blog* ${IP_RANGE_BASE}:6600
             header {
-                Access-Control-Allow-Origin https://onton.live
+                Access-Control-Allow-Origin https://onton.live, https://blog.onton.live, https://app.onton.live
                 Access-Control-Allow-Methods GET, POST, OPTIONS, PUT, DELETE
                 Access-Control-Allow-Headers Content-Type, Authorization
                 Access-Control-Allow-Credentials true
