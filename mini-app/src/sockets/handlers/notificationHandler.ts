ec4a736bd2ffdb0200b35eadad33530cb72a20df
radiophp <radiophp@gmail.com>
2024-12-06 11:36:22 +0300

    add: notification reply action

--- mini-app/src/sockets/handlers/notificationHandler.ts
+++ mini-app/src/sockets/handlers/notificationHandler.ts
@@ -6,6 +6,7 @@ import { sanitizeString } from "@/lib/sanitizer";
 import { handleDisconnection } from "./handleDisconnection";
 import { handleTestEvent } from "./handleTestEvent";
 import { handleUnknownEvent } from "./handleUnknownEvent";
+import { handleNotificationReply } from "./notificationReply";
 // Track active user connections
 
 
@@ -32,7 +33,12 @@ export const handleNotifications = async (io: Server) => {
       userSockets.set(user.id, new Set());
     }
     userSockets.get(user.id)?.add(socket.id);
-
+    // Handle notification reply event
+    // Expecting client to emit: socket.emit("notificationReply", { notificationId: "123", answer: "yes" }, (response) => { ... });
+    socket.on(SocketEvents.receive.notificationReply, (data, callback) => {
+      // Use the handler to validate and process the reply
+      handleNotificationReply(data, callback, sanitizedUsername , user.id);
+    });
     // Test event handler
     socket.on(SocketEvents.receive.test, (data, callback) => {
       handleTestEvent(data, callback, sanitizedUsername);
