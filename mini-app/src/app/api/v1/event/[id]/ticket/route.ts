9863525b17d53b19f3b6f6674e89d40444950d67
alirezaprf <alirezaadvertise@gmail.com>
2024-12-11 13:25:44 +0330

    --- 🤬👿💀 Comment Errors Of API

--- mini-app/src/app/api/v1/event/[id]/ticket/route.ts
+++ mini-app/src/app/api/v1/event/[id]/ticket/route.ts
@@ -1,45 +1,45 @@
-import { db } from "@/db/db";
-import { eventTicket, tickets } from "@/db/schema";
-import { getAuthenticatedUser } from "@/server/auth";
-import { and, eq, or, desc, asc } from "drizzle-orm";
+// import { db } from "@/db/db";
+// import { eventTicket, tickets } from "@/db/schema";
+// import { getAuthenticatedUser } from "@/server/auth";
+// import { and, eq, or, desc, asc } from "drizzle-orm";
 
-export async function GET(_: Request, { params }: { params: { id: string } }) {
-  const eventId = params.id;
+// export async function GET(_: Request, { params }: { params: { id: string } }) {
+//   const eventId = params.id;
 
-  const [userId, unauthorized] = getAuthenticatedUser();
+//   const [userId, unauthorized] = getAuthenticatedUser();
 
-  if (unauthorized) {
-    return unauthorized;
-  }
+//   if (unauthorized) {
+//     return unauthorized;
+//   }
 
-  const ticket = (
-    await db
-      .select()
-      .from(tickets)
-      .where(and(eq(tickets.event_uuid, eventId), eq(tickets.user_id, userId), eq(tickets.status, "UNUSED")))
-      .orderBy(desc(tickets.updatedAt))
+//   const ticket = (
+//     await db
+//       .select()
+//       .from(tickets)
+//       .where(and(eq(tickets.event_uuid, eventId), eq(tickets.user_id, userId), eq(tickets.status, "UNUSED")))
+//       .orderBy(desc(tickets.updatedAt))
 
-      .execute()
-  ).pop();
+//       .execute()
+//   ).pop();
 
-  if (!ticket) {
-    // ticket not found error
-    return Response.json({ error: "Ticket not found" }, { status: 404 });
-  }
+//   if (!ticket) {
+//     // ticket not found error
+//     return Response.json({ error: "Ticket not found" }, { status: 404 });
+//   }
 
-  const eventTicketData = (
-    await db.select().from(eventTicket).where(eq(eventTicket.id, ticket.ticket_id)).execute()
-  ).pop();
+//   const eventTicketData = (
+//     await db.select().from(eventTicket).where(eq(eventTicket.id, ticket.ticket_id)).execute()
+//   ).pop();
 
-  if (!eventTicketData) {
-    // ticket not found error
-    return Response.json({ error: "Ticket data not found" }, { status: 400 });
-  }
+//   if (!eventTicketData) {
+//     // ticket not found error
+//     return Response.json({ error: "Ticket data not found" }, { status: 400 });
+//   }
 
-  const data = {
-    ...ticket,
-    ticketData: eventTicketData,
-  };
+//   const data = {
+//     ...ticket,
+//     ticketData: eventTicketData,
+//   };
 
-  return Response.json(data);
-}
+//   return Response.json(data);
+// }
