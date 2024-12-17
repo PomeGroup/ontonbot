cc2c72165cf55ef94b4dce03cc9638627aa26a81
samyar <samyarkd@gmail.com>
2024-12-17 19:39:26 +0330

    fix: add ton connect orders

--- mini-app/src/app/_components/Event/Orders/Orders.tsx
+++ mini-app/src/app/_components/Event/Orders/Orders.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import ListLayout from "../../atoms/cards/ListLayout";
 import { useGetEventOrders } from "@/hooks/events.hooks";
 import { SiTon } from "react-icons/si";
-import { useTonConnectUI, useTonWallet } from "@tonconnect/ui-react";
+import { TonConnectButton, useTonConnectUI, useTonWallet } from "@tonconnect/ui-react";
 import { useConfig } from "@/context/ConfigContext";
 import { beginCell, toNano } from "@ton/core";
 import { toast } from "sonner";
@@ -21,90 +21,96 @@ const EventOrders = () => {
   const tonWallet = useTonWallet();
 
   return (
-    <ListLayout
-      isLoading={orders.isLoading}
-      isEmpty={orders.data?.length === 0 || orders.isError}
-      title="Orders List"
-    >
-      {orders.data?.map((order) => {
-        return (
-          <ListItem
-            key={order.uuid}
-            /**
-             * ORDER TITLE
-             */
-            title={
-              <span className="flex gap-2 items-center">
-                <b className="font-extrabold antialiased">{order.total_price}</b> <SiTon className="text-sky-600" />
-              </span>
-            }
-            /**
-             * ORDER DESCRIPTION
-             */
-            footer={
-              <div className="flex flex-col mt-2 gap-2">
-                <p className="capitalize ">
-                  <b className="font-semibold antialiased">{order.order_type.replaceAll("_", " ")}</b>:{" "}
-                  {order.order_type === "event_creation" && (
-                    <span>Event won&#39;t be created unless this order is paid</span>
+    <>
+      <div className="flex items-center justify-center">
+        <TonConnectButton />
+      </div>
+
+      <ListLayout
+        isLoading={orders.isLoading}
+        isEmpty={orders.data?.length === 0 || orders.isError}
+        title="Orders List"
+      >
+        {orders.data?.map((order) => {
+          return (
+            <ListItem
+              key={order.uuid}
+              /**
+               * ORDER TITLE
+               */
+              title={
+                <span className="flex gap-2 items-center">
+                  <b className="font-extrabold antialiased">{order.total_price}</b> <SiTon className="text-sky-600" />
+                </span>
+              }
+              /**
+               * ORDER DESCRIPTION
+               */
+              footer={
+                <div className="flex flex-col mt-2 gap-2">
+                  <p className="capitalize ">
+                    <b className="font-semibold antialiased">{order.order_type.replaceAll("_", " ")}</b>:{" "}
+                    {order.order_type === "event_creation" && (
+                      <span>Event won&#39;t be created unless this order is paid</span>
+                    )}
+                    {order.order_type === "event_capacity_increment" && (
+                      <span>
+                        Increase event capacity by <b>{order.total_price / 0.055}</b> tickets
+                      </span>
+                    )}
+                  </p>
+                  {order.state === "created" && (
+                    <Button
+                      onClick={() => {
+                        tonWallet?.account.address
+                          ? // FIXME: support for usdt
+                            tonConnectUI
+                              .sendTransaction({
+                                validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec
+                                messages: [
+                                  {
+                                    amount: toNano(order.total_price).toString(),
+                                    address: config.ONTON_WALLET_ADDRESS!,
+                                    payload: beginCell()
+                                      .storeUint(0, 32)
+                                      .storeStringTail(`onton_order=${order.uuid}`)
+                                      .endCell()
+                                      .toBoc()
+                                      .toString("base64"),
+                                  },
+                                ],
+                              })
+                              .then(() => {
+                                toast("Please wait until we confirm the transaction and do not pay again");
+                                trpcUtils.events.getEventOrders.setData(
+                                  {
+                                    event_uuid: order.uuid,
+                                  },
+                                  (oldData) => {
+                                    if (!oldData) return oldData;
+                                    return oldData.map((item) =>
+                                      item.uuid === order.uuid ? { ...item, state: "processing" } : item
+                                    );
+                                  }
+                                );
+                              })
+                          : tonConnectUI.openModal();
+                      }}
+                    >
+                      {tonConnectUI.account?.address ? "Pay" : "Connect Wallet"}
+                    </Button>
                   )}
-                  {order.order_type === "event_capacity_increment" && (
-                    <span>
-                      Increase event capacity by <b>{order.total_price / 0.055}</b> tickets
-                    </span>
-                  )}
-                </p>
-                {order.state === "created" && (
-                  <Button
-                    onClick={() => {
-                      tonWallet?.account.address
-                        ? // FIXME: support for usdt
-                          tonConnectUI
-                            .sendTransaction({
-                              validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec
-                              messages: [
-                                {
-                                  amount: toNano(order.total_price).toString(),
-                                  address: config.ONTON_WALLET_ADDRESS!,
-                                  payload: beginCell()
-                                    .storeUint(0, 32)
-                                    .storeStringTail(`onton_order=${order.uuid}`)
-                                    .endCell()
-                                    .toBoc()
-                                    .toString("base64"),
-                                },
-                              ],
-                            })
-                            .then(() => {
-                              toast("Please wait until we confirm the transaction and do not pay again");
-                              trpcUtils.events.getEventOrders.setData(
-                                {
-                                  event_uuid: order.uuid,
-                                },
-                                (oldData) => {
-                                  if (!oldData) return oldData;
-                                  return oldData.map((item) =>
-                                    item.uuid === order.uuid ? { ...item, state: "processing" } : item
-                                  );
-                                }
-                              );
-                            })
-                        : tonConnectUI.openModal();
-                    }}
-                  >
-                    {tonConnectUI.account?.address ? "Pay" : "Connect Wallet"}
-                  </Button>
-                )}
-              </div>
-            }
-            /**
-             * HANDLE ORDER STATUS
-             */
-            after={<p className="capitalize">{order.state}</p>}
-          />
-        );
-      })}
-    </ListLayout>
+                </div>
+              }
+              /**
+               * HANDLE ORDER STATUS
+               */
+              after={<p className="capitalize">{order.state}</p>}
+            />
+          );
+        })}
+      </ListLayout>
+    </>
   );
 };
 
