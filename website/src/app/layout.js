4bf121d314293596a05a6f3de86c77ffd1a07061
samyar <samyarkd@gmail.com>
2024-12-18 10:09:16 +0330

    fix: add tag manager to website

--- website/src/app/layout.js
+++ website/src/app/layout.js
@@ -1,26 +1,28 @@
 import Footer from "@/components/layout/Footer";
 import Navbar from "@/components/layout/Navbar";
 import { Inter } from "next/font/google";
+import { GoogleTagManager } from "@next/third-parties/google";
+
 import "./globals.css";
-import { headers } from "next/headers"; // Access HTTP headers
-import { redirect } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: "ONton Landing Page",
-    description: "ONton - Experience the Future of Event Management",
+  title: "ONton Landing Page",
+  description: "ONton - Experience the Future of Event Management",
 };
 
 export default function RootLayout({ children }) {
-
-    return (
-        <html lang="en">
-        <body className={inter.className}>
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {process.env.ENV === "poduction" && (
+          <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
+        )}
         <Navbar />
         <main>{children}</main>
         <Footer />
-        </body>
-        </html>
-    );
+      </body>
+    </html>
+  );
 }
