9548645b64aabf23512b2778d65e54aa742c086d
alirezaprf <alirezaadvertise@gmail.com>
2024-12-20 17:00:17 +0330

    added env utils - 0.1 ton for event creation on dev

---
+++ mini-app/src/server/utils/evnutils.ts
@@ -0,0 +1,9 @@
+export function is_dev_env(){
+    return process.env.NODE_ENV === "development";
+}
+export function is_stage_env(){
+    return process.env.NODE_ENV === "staging";
+}
+export function is_prod_env(){
+    return process.env.NODE_ENV === "production";
+}
