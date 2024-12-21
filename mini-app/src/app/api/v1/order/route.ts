6fc4f0c8c5d4b9470b58bcbb55230465e53f62b3
alirezaprf <alirezaadvertise@gmail.com>
2024-12-21 15:19:06 +0330

    fix order

--- mini-app/src/app/api/v1/order/route.ts
+++ mini-app/src/app/api/v1/order/route.ts
@@ -71,12 +71,7 @@ export async function POST(request: Request) {
     await db
       .select()
       .from(orders)
-      .where(and(
-        eq(orders.user_id, userId), 
-        eq(orders.order_type, "nft_mint"),
-        eq(orders.event_uuid, eventData.event_uuid),
-      
-      ))
+      .where(and(eq(orders.user_id, userId), eq(orders.order_type, "nft_mint"), eq(orders.event_uuid, eventData.event_uuid)))
       .execute()
   ).pop();
 
@@ -92,14 +87,11 @@ export async function POST(request: Request) {
       });
     }
 
-    return Response.json(
-      {
-        message: "An order is already being proccessed",
-      },
-      {
-        status: 409,
-      }
-    );
+    return Response.json({
+      order_id: userOrder.uuid,
+      message: "An order is already being proccessed",
+      payment_type: userOrder.payment_type,
+    });
   }
 
   if (TicketsCount[0].count >= (eventData.capacity || 0)) {
