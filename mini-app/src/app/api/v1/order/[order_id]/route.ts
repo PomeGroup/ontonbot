f51c8fb131d65759f5e67df01a5c406f62eb6cc8
alirezaprf <alirezaadvertise@gmail.com>
2024-12-22 10:18:32 +0330

    fix v1/order/order_id

--- mini-app/src/app/api/v1/order/[order_id]/route.ts
+++ mini-app/src/app/api/v1/order/[order_id]/route.ts
@@ -2,10 +2,10 @@ import { db } from "@/db/db";
 import { orders, tickets } from "@/db/schema";
 import { sendLogNotification } from "@/lib/tgBot";
 import { apiKeyAuthentication, getAuthenticatedUser } from "@/server/auth";
-import { Address } from "@ton/core";
-import { eq, sql } from "drizzle-orm";
 import { NextRequest } from "next/server";
 import { z } from "zod";
+
+
 
 type OptionsProps = {
   params: {
@@ -14,44 +14,46 @@ type OptionsProps = {
 };
 
 export async function GET(req: NextRequest, { params }: OptionsProps) {
-  return Response.json({});
-  // const orderId = params.order_id;
+  const orderId = params.order_id;
 
-  // const [, error] = getAuthenticatedUser();
-  // const apiKeyError = apiKeyAuthentication(req);
-  // if (error && apiKeyError) return error || apiKeyError;
+  const [, error] = getAuthenticatedUser();
+  const apiKeyError = apiKeyAuthentication(req);
+  if (error && apiKeyError) return error || apiKeyError;
 
-  // const order = await db.query.orders.findFirst({
-  //   where(fields, { eq }) {
-  //     return eq(fields.uuid, orderId);
-  //   },
-  // });
+  const order = await db.query.orders.findFirst({
+    where(fields, { eq }) {
+      return eq(fields.uuid, orderId);
+    },
+  });
 
-  // if (!order) {
-  //   return Response.json({ message: "order_not_found" }, { status: 404 });
-  // }
+  if (!order) {
+    return Response.json({ message: "order_not_found" }, { status: 404 });
+  }
+  if (!order.event_uuid) {
+    return Response.json({ message: "order_does_not_have_event_uuid" }, { status: 400 });
+  }
 
-  // const tickets = await db.query.tickets.findMany({
-  //   where(fields, { eq }) {
-  //     return eq(fields.order_uuid, order.uuid);
-  //   },
-  // });
+  const tickets = await db.query.tickets.findMany({
+    where(fields, { eq }) {
+      return eq(fields.order_uuid, order.uuid);
+    },
+  });
 
-  // // get event ticket and if not found return event ticket not found
-  // const eventTicketData = await db.query.eventTicket.findFirst({
-  //   where(fields, { eq }) {
-  //     return eq(fields.id, order.event_ticket_id);
-  //   },
-  // });
+  // get event ticket and if not found return event ticket not found
+  const eventPaymentInfo = await db.query.eventPayment.findFirst({
+    where(fields, { eq }) {
+      return eq(fields.event_uuid, order.event_uuid!);
+    },
+  });
 
-  // if (!eventTicketData) return Response.json({ message: "event_ticket_not_found" }, { status: 404 });
+  if (!eventPaymentInfo) return Response.json({ message: "event_ticket_not_found" }, { status: 404 });
 
-  // return Response.json({
-  //   ...order,
-  //   total_price: BigInt(order.total_price as bigint).toString(),
-  //   nft_collection_address: eventTicketData.collectionAddress,
-  //   tickets,
-  // });
+  return Response.json({
+    ...order,
+    total_price: BigInt(order.total_price).toString(),
+    nft_collection_address: eventPaymentInfo.collectionAddress,
+    tickets,
+  });
 }
 
 
