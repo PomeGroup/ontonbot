332cea045f31cbf55ee7d076051ea5b73100579d
Masoud Bonabi <masoudbonabi@gmail.com>
2024-12-25 15:10:21 +0330

    Fixes opening Drawer for the second time

--- mini-app/src/components/ui/drawer.tsx
+++ mini-app/src/components/ui/drawer.tsx
@@ -45,7 +45,10 @@ const DrawerContent = React.forwardRef<
     <DrawerOverlay />
     <DrawerPrimitive.Content
       ref={ref}
-      className={cn("fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-3xl bg-cn-background p-4", className)}
+      className={cn(
+        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-3xl bg-cn-background p-4",
+        className
+      )}
       {...props}
     >
       {/* Conditionally render the DrawerClose button */}
@@ -78,15 +81,16 @@ const DrawerFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivEleme
 );
 DrawerFooter.displayName = "DrawerFooter";
 
-const DrawerTitle = React.forwardRef<React.ElementRef<typeof DrawerPrimitive.Title>, React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Title>>(
-  ({ className, ...props }, ref) => (
-    <DrawerPrimitive.Title
-      ref={ref}
-      className={cn("text-xl font-bold leading-none tracking-tight", className)}
-      {...props}
-    />
-  )
-);
+const DrawerTitle = React.forwardRef<
+  React.ElementRef<typeof DrawerPrimitive.Title>,
+  React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Title>
+>(({ className, ...props }, ref) => (
+  <DrawerPrimitive.Title
+    ref={ref}
+    className={cn("text-xl font-bold leading-none tracking-tight", className)}
+    {...props}
+  />
+));
 DrawerTitle.displayName = DrawerPrimitive.Title.displayName;
 
 const DrawerDescription = React.forwardRef<
@@ -146,7 +150,10 @@ export const KSheet = (props: KSheeProps) => {
         ))}
       {createPortal(
         <Sheet
-          onBackdropClick={() => setOpen(false)}
+          onBackdropClick={() => {
+            props.onOpenChange?.(false);
+            setOpen(false);
+          }}
           className="w-full"
           opened={open}
         >
@@ -158,4 +165,15 @@ export const KSheet = (props: KSheeProps) => {
   );
 };
 
-export { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerOverlay, DrawerPortal, DrawerTitle, DrawerTrigger };
+export {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerPortal,
+  DrawerTitle,
+  DrawerTrigger,
+};
