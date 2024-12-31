8331367c1991193a5fb958ffea61d66a19e0660e
alirezaprf <alirezaadvertise@gmail.com>
2024-12-31 07:58:24 +0330

    full deploy

--- mini-app/src/start.ts
+++ mini-app/src/start.ts
@@ -283,7 +283,7 @@ async function CheckTransactions(pushLockTTl: () => any) {
             eq(orders.uuid, o.order_uuid),
             or(eq(orders.state, "new"), eq(orders.state, "confirming")),
             eq(orders.total_price, o.value),
-            eq(orders.payment_type , o.order_type)
+            eq(orders.payment_type, o.order_type)
           )
         );
     }
