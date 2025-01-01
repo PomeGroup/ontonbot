7070d1f7f78e12d05cee28256d66b3f18ec99228
samyar <samyarkd@gmail.com>
2025-01-01 19:47:25 +0330

    fix: not

--- newton/apps/participant-tma/utils/constants.ts
+++ newton/apps/participant-tma/utils/constants.ts
@@ -1,6 +1,6 @@
 import { Address } from "@ton/ton";
 
-export const isTestnet = !["production", "stage"].includes(process.env.ENV!);
+export const isTestnet = !(/*NOT*/ ["production", "stage", "staging"].includes(process.env.ENV!));
 
 export const USDT_MASTER_ADDRESS = Address.parse(
   isTestnet ? "kQD0GKBM8ZbryVk2aESmzfU6b9b_8era_IkvBSELujFZPsyy" : "EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs"
