7fe3b9edad86ab80720775b928116003eebb5f3a
Masoud Bonabi <masoudbonabi@gmail.com>
2025-01-03 12:39:28 +0330

    Brings back providers for testing'

--- mini-app/src/app/providers.tsx
+++ mini-app/src/app/providers.tsx
@@ -3,7 +3,7 @@
 import { Toaster } from "@/components/ui/toaster";
 import { ConfigProvider } from "@/context/ConfigContext";
 import { NavigationHistoryProvider } from "@/context/NavigationHistoryContext";
-import { TonConnectUIProvider } from "@tonconnect/ui-react";
+// import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import { ThemeProvider } from "next-themes";
 import React, { useEffect } from "react";
 import WebAppProvider from "./_components/WebAppProvider";
@@ -22,34 +22,34 @@ const Providers = ({ children, isDevStage }: { children: React.ReactNode; isDevS
 
   return (
     <Sentry.ErrorBoundary>
-      <TonConnectUIProvider
+      {/* <TonConnectUIProvider
         actionsConfiguration={{
           twaReturnUrl: `https://t.me/${process.env.NEXT_PUBLIC_BOT_USERNAME}/event`,
         }}
         manifestUrl="https://gist.githubusercontent.com/nichitagutu/3cc22ee9749e77222c38313de47c94bc/raw/f37de28e672932101702f841d02d7414b93ca9ac/tonconnect-manifest.json"
+      > */}
+      <ThemeProvider
+        defaultTheme="light"
+        attribute="class"
       >
-        <ThemeProvider
-          defaultTheme="light"
-          attribute="class"
-        >
-          <WebAppProvider>
-            <TRPCAPIProvider>
-              <NavigationHistoryProvider>
-                <ConfigProvider>
-                  <KonstaAppProvider>
-                    <ThemeSetter>
-                      <NotificationProvider>
-                        <UserSaver>{children}</UserSaver>
-                      </NotificationProvider>
-                    </ThemeSetter>
-                    <Toaster />
-                  </KonstaAppProvider>
-                </ConfigProvider>
-              </NavigationHistoryProvider>
-            </TRPCAPIProvider>
-          </WebAppProvider>
-        </ThemeProvider>
-      </TonConnectUIProvider>
+        <WebAppProvider>
+          <TRPCAPIProvider>
+            <NavigationHistoryProvider>
+              <ConfigProvider>
+                <KonstaAppProvider>
+                  <ThemeSetter>
+                    <NotificationProvider>
+                      <UserSaver>{children}</UserSaver>
+                    </NotificationProvider>
+                  </ThemeSetter>
+                  <Toaster />
+                </KonstaAppProvider>
+              </ConfigProvider>
+            </NavigationHistoryProvider>
+          </TRPCAPIProvider>
+        </WebAppProvider>
+      </ThemeProvider>
+      {/* </TonConnectUIProvider> */}
     </Sentry.ErrorBoundary>
   );
 };
