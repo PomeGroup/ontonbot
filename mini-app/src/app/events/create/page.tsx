90b45b18483b2c18b782d5d8b550900f15078e61
masious <masoudbonabi@gmail.com>
2025-01-23 16:20:55 +0330

    Removes useAuth that used hasAccessToAdministration api

--- mini-app/src/app/events/create/page.tsx
+++ mini-app/src/app/events/create/page.tsx
@@ -2,22 +2,10 @@
 
 import Alerts from "@/app/_components/molecules/alerts";
 import ManageEvent from "@/app/_components/organisms/events/ManageEvent";
-import useAuth from "@/hooks/useAuth";
 import { Page } from "konsta/react";
 
 
-export default function CreateEventAdminPage ()  {
-  const { authorized, isLoading } = useAuth();
-
-  if (isLoading) {
-    return null;
-  }
-
-  if (authorized === false) {
-    return <Alerts.NotAuthorized />;
-  }
-
-
+export default function CreateEventAdminPage() {
   return (
     <Page className="min-h-screen">
       <ManageEvent />
