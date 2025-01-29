1469e13ead7aa9e5b57d1201bb2a4edde6b8ebf9
samyar <samyarkd@gmail.com>
2025-01-29 14:12:28 +0200

    fix: diffrent state and refactor the orders page

--- mini-app/src/app/events/[hash]/manage/orders/page.tsx
+++ mini-app/src/app/events/[hash]/manage/orders/page.tsx
@@ -4,13 +4,10 @@ import React from "react";
 import { Page, Block } from "konsta/react";
 import EventOrders from "@/app/_components/Event/Orders/Orders";
 
-
 export default function OrdersPage() {
-
-
   return (
     <Page>
-      <Block className="px-4 pt-4">
+      <Block>
         <h1 className="text-lg font-bold">Event Orders</h1>
       </Block>
       {/* We can use `eventData` if needed, or just show <EventOrders/> */}
