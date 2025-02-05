7f0bba57c0f9f7fe6ebb23e00b01e27ef6ecdea2
Alireza Prf <33633327+alirezaprf@users.noreply.github.com>
2025-02-05 12:19:54 +0330

    lanat be cache

--- telegram-bot/src/utils/onton-config.ts
+++ telegram-bot/src/utils/onton-config.ts
@@ -8,18 +8,22 @@ let configProtected: { [key: string]: string | null } = {};
 
 (async () => {
   try {
-    const cachedConfig = await getCache(cacheKeys.ontonSettings);
-    const cachedConfigProtected = await getCache(cacheKeys.ontonSettingsProtected);
+    // const cachedConfig = await getCache(cacheKeys.ontonSettings);
+    // const cachedConfigProtected = await getCache(cacheKeys.ontonSettingsProtected);
 
-    // If both configurations are cached, return them immediately
-    if (cachedConfig && cachedConfigProtected) {
-      return { config: cachedConfig, configProtected: cachedConfigProtected };
-    }
+    // // If both configurations are cached, return them immediately
+    // if (cachedConfig && cachedConfigProtected) {
+    //   return { config: cachedConfig, configProtected: cachedConfigProtected };
+    // }
     // Fetch the settings and restructure into config and configProtected
     const ontonConfig = await fetchOntonSetting();
 
     config = ontonConfig.config;
     configProtected = ontonConfig.configProtected;
+    
+    logger.log('configProtected' , configProtected);
+    logger.log('config' , config);
+
   } catch (error) {
     logger.error("Error fetching Onton settings:", error);
     // Handle the error or use default configurations
