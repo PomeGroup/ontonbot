ca4a299316656338a42def24229891ea39a76bbb
radiophp <radiophp@gmail.com>
2025-02-28 20:20:36 +0300

    confilit: resolve migration confilicts

--- mini-app/drizzle/0010_giant_zombie.sql
+++
@@ -1,9 +0,0 @@
-CREATE TYPE ton_society_status_enum AS ENUM (
-    'NOT_CLAIMED',
-    'CLAIMED',
-    'NOT_ELIGIBLE',
-    'RECEIVED'
-    );
-
-ALTER TABLE "rewards"
-    ADD COLUMN "ton_society_status" "ton_society_status_enum" DEFAULT 'NOT_CLAIMED' NOT NULL;
