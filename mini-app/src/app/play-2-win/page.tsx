00717cc9403cc2cbb17ef099d6bb1079bccc7edc
samyar <samyarkd@gmail.com>
2025-03-12 15:49:39 +0330

    feat: add featured tournaments section with Swiper integration

--- mini-app/src/app/play-2-win/page.tsx
+++ mini-app/src/app/play-2-win/page.tsx
@@ -9,10 +9,90 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React from "react";
 import "swiper/css";
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 import CustomCard from "../_components/atoms/cards/CustomCard";
 import CustomButton from "../_components/Button/CustomButton";
 import DataStatus from "../_components/molecules/alerts/DataStatus";
 import { trpc } from "../_trpc/client";
+
+interface TournamentCardProps {
+  tournamentId: string;
+}
+
+const TournamentCard: React.FC<TournamentCardProps> = ({ tournamentId }) => {
+  const router = useRouter();
+
+  const queryEnabeld = !isNaN(Number(tournamentId));
+  const tournament = trpc.tournaments.getTournamentById.useQuery(
+    { id: Number(tournamentId) },
+    {
+      enabled: queryEnabeld,
+    }
+  );
+
+  if (tournament.isError || !queryEnabeld) {
+    return null;
+  }
+
+  return (
+    <SwiperSlide
+      key={tournamentId}
+      className="!w-[220px]"
+    >
+      {tournament.isLoading ? (
+        <Skeleton className="w-[220px] h-[220px] rounded-md" />
+      ) : (
+        <Image
+          src={tournament.data?.imageUrl}
+          width={220}
+          height={220}
+          onClick={() => {
+            router.push(`/play-2-win/${tournamentId}`);
+          }}
+          className="w-[220px] rounded-md hover:cursor-pointer"
+          alt="tournament image card"
+        />
+      )}
+    </SwiperSlide>
+  );
+};
+
+const Play2WinFeatured = () => {
+  const config = trpc.config.getConfig.useQuery();
+  // Get the featured events from the config: tId,tId,tId
+  const play2winFeaturedEvents = config.data?.config["play-2-win-featured"];
+  const parsedFeaturedEvents =
+    typeof play2winFeaturedEvents === "string" ? play2winFeaturedEvents?.split(",").map((tId) => tId.trim()) : undefined;
+
+  if (!parsedFeaturedEvents?.length) {
+    return null;
+  }
+
+  return (
+    <>
+      <Typography variant="title2">Featured Contests</Typography>
+      <div>
+        <Swiper
+          slidesPerView="auto"
+          className="-mx-3"
+          spaceBetween={12}
+          pagination={{ clickable: true }}
+          autoHeight
+          modules={[Pagination]}
+          wrapperClass="swiper-wrapper pb-3 px-4"
+        >
+          {parsedFeaturedEvents.map((tId) => (
+            <TournamentCard
+              tournamentId={tId}
+              key={tId}
+            />
+          ))}
+        </Swiper>
+      </div>
+    </>
+  );
+};
 
 const PlayToWin: React.FC = () => {
   const router = useRouter();
@@ -31,38 +111,7 @@ const PlayToWin: React.FC = () => {
   return (
     <Page>
       <div className="p-4 flex flex-col gap-2 mb-12">
-        {/* <Typography variant="title2">Featured Contests</Typography>
-        <div>
-          <Swiper
-            slidesPerView="auto"
-            className="-mx-3"
-            spaceBetween={12}
-            pagination={{ clickable: true }}
-            autoHeight
-            modules={[Pagination]}
-            wrapperClass="swiper-wrapper pb-3 px-4"
-          >
-            <SwiperSlide className="!w-[220px]">
-              <Image
-                src="https://s3-alpha-sig.figma.com/img/1e4c/449e/2570818513b584eeb180eaadace966f7?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=EGzjtweOC4U3JDfHqwxE6wHMlvKOqJ2XFAjHkDw58Jv8kYGB-koNwkXSlevC8V0DKBalxTvtaEaja0pCtOXNe5lQaVudFzHgfkJzJdug4UcXI~0u1BSLkJ31UE0JXqEVqq2sl5NXH5WG8IL0l8Pm5SPRsbMT3FflSLqO~~MP8RE3lhXjr7L2VD9upfsb5o25iu7L2aSXY2AUfnlSF1n~rfy576w0xHVppHsJQDgHOq2l0zEVhXSjAkYFK9rJPeL7YdTElO2ODk-2awveH6P1Xu75spLEhyTus4mVv9xiTa8HpIHnW8C0YvZaCqENRGYJ0FFWxPqzzXHMAgzkbActVQ__"
-                width={220}
-                height={220}
-                className="w-[220px] rounded-md"
-                alt="game card"
-              />
-            </SwiperSlide>
-            <SwiperSlide className="w-[220px]">
-              <Image
-                src="https://s3-alpha-sig.figma.com/img/1e4c/449e/2570818513b584eeb180eaadace966f7?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=EGzjtweOC4U3JDfHqwxE6wHMlvKOqJ2XFAjHkDw58Jv8kYGB-koNwkXSlevC8V0DKBalxTvtaEaja0pCtOXNe5lQaVudFzHgfkJzJdug4UcXI~0u1BSLkJ31UE0JXqEVqq2sl5NXH5WG8IL0l8Pm5SPRsbMT3FflSLqO~~MP8RE3lhXjr7L2VD9upfsb5o25iu7L2aSXY2AUfnlSF1n~rfy576w0xHVppHsJQDgHOq2l0zEVhXSjAkYFK9rJPeL7YdTElO2ODk-2awveH6P1Xu75spLEhyTus4mVv9xiTa8HpIHnW8C0YvZaCqENRGYJ0FFWxPqzzXHMAgzkbActVQ__"
-                width={220}
-                height={220}
-                className="w-[220px] rounded-md"
-                alt="game card"
-              />
-            </SwiperSlide>
-          </Swiper>
-        </div> */}
-
+        <Play2WinFeatured />
         <Typography variant="title2">Discover</Typography>
         <div className="grid grid-cols-2 gap-4">
           {!tournomants.data?.pages[0].tournaments.length && (
