c8b3cae7db4cb50d451907f0d4e22dd418103c88
samyar <samyarkd@gmail.com>
2025-03-19 12:23:58 +0330

    fix: update Storybook addons configuration and add Vitest setup

--- mini-app/.storybook/main.ts
+++ mini-app/.storybook/main.ts
@@ -5,12 +5,12 @@ const config: StorybookConfig = {
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "addons": [
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
-  ],
+  "addons": [{
+    "name": "@storybook/addon-essentials",
+    "options": {
+      "docs": false
+    }
+  }, "@storybook/addon-onboarding", "@chromatic-com/storybook", "@storybook/experimental-addon-test"],
   "framework": {
     "name": "@storybook/experimental-nextjs-vite",
     "options": {}
