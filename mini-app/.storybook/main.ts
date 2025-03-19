8124d4b9b989f800277f1f294b5eaa84ed1850bc
samyar <samyarkd@gmail.com>
2025-03-19 16:45:45 +0330

    feat: add storybook

--- mini-app/.storybook/main.ts
+++ mini-app/.storybook/main.ts
@@ -1,22 +1,23 @@
 import type { StorybookConfig } from "@storybook/experimental-nextjs-vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    {
+      name: "@storybook/addon-essentials",
+      options: {
+        docs: false,
+      },
+    },
+    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook",
+    "@storybook/experimental-addon-test",
+    "@storybook/addon-styling-webpack",
   ],
-  "addons": [{
-    "name": "@storybook/addon-essentials",
-    "options": {
-      "docs": false
-    }
-  }, "@storybook/addon-onboarding", "@chromatic-com/storybook", "@storybook/experimental-addon-test"],
-  "framework": {
-    "name": "@storybook/experimental-nextjs-vite",
-    "options": {}
+  framework: {
+    name: "@storybook/experimental-nextjs-vite",
+    options: {},
   },
-  "staticDirs": [
-    "../public"
-  ]
+  staticDirs: ["../public"],
 };
 export default config;
--- mini-app/.storybook/preview.ts
+++ mini-app/.storybook/preview.ts
@@ -1,11 +1,13 @@
-import type { Preview } from '@storybook/react'
+import type { Preview } from "@storybook/react";
+import "../src/app/page.css";
 
 const preview: Preview = {
   parameters: {
+    layout: "centered",
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
