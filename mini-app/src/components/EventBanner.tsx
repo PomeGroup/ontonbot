7f46dcff140850820ade6656ecf8d312a3be4dcb
samyar <samyarkd@gmail.com>
2025-03-21 15:25:45 +0330

    refactor: replace Image component with LoadableImage in EventBanner

--- mini-app/src/components/EventBanner.tsx
+++ mini-app/src/components/EventBanner.tsx
@@ -2,9 +2,8 @@ import useWebApp from "@/hooks/useWebApp";
 import { isValidImageUrl } from "@/lib/isValidImageUrl";
 import { OntonEvent } from "@/types";
 import { Skeleton } from "@mui/material";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import LoadableImage from "./LoadableImage";
 
 type SkeletonProps = { skeleton: true; event?: null };
 type EventProps = {
@@ -21,7 +20,6 @@ const defaultImage = "/template-images/default.webp";
 
 const emptyObj = {} as any;
 export default function EventBanner({ skeleton, event, className }: Props) {
-  const [imageLoaded, setImageLoaded] = useState(false);
   const webApp = useWebApp();
   const router = useRouter();
 
@@ -55,16 +53,15 @@ export default function EventBanner({ skeleton, event, className }: Props) {
       key={`detailed-${eventUuid}`}
     >
       {!skeleton && (
-        <Image
+        <LoadableImage
           // if date now before 5 november 2024 show special image
           src={isValidImageUrl(imageUrl) ? imageUrl : defaultImage}
           alt={title}
           width={0}
           height={0}
           sizes="100vw"
-          className={`aspect-square w-[220px] rounded-[10px] transition-opacity duration-250 ${imageLoaded ? "opacity-100" : "opacity-0"}`}
+          className={`aspect-square w-[220px] rounded-[10px] transition-opacity duration-250`}
           onError={(e) => (e.currentTarget.src = defaultImage)}
-          onLoad={() => setImageLoaded(true)}
         />
       )}
     </div>
