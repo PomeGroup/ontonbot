cc107a23a449512b1a12a9664b6b71bf0dfe2951
radiophp <radiophp@gmail.com>
2025-04-06 23:10:12 +0300

    fix: fucking shit user scores

--- mini-app/src/cronJobs/helper/maybeInsertUserScore.ts
+++ mini-app/src/cronJobs/helper/maybeInsertUserScore.ts
@@ -41,13 +41,13 @@ export const maybeInsertUserScore = async (userId: number, eventId: number) => {
   } else if (isPaid && !isOnline) {
     // participationType could be "in_person" or some variation
     chosenActivityType = "paid_offline_event";
-    points = 10;
+    points = 20;
   } else if (!isPaid && isOnline) {
     chosenActivityType = "free_online_event";
-    points = 2;
+    points = 1;
   } else {
     chosenActivityType = "free_offline_event";
-    points = 2;
+    points = 10;
   }
 
   // 3) Insert user score if not exists
