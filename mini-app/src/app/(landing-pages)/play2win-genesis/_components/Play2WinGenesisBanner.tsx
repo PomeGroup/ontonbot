22d3937bb18c19f8e39789348f6eea5b935ef9dc
samyar <samyarkd@gmail.com>
2025-04-25 01:38:43 +0330

    fix: remove p2w banner

--- mini-app/src/app/(landing-pages)/play2win-genesis/_components/Play2WinGenesisBanner.tsx
+++ mini-app/src/app/(landing-pages)/play2win-genesis/_components/Play2WinGenesisBanner.tsx
@@ -2,17 +2,16 @@ import { cn } from "@/utils";
 import Image from "next/image";
 import Link from "next/link";
 
-export const Play2WinGenesisBanner: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <Link
-    href="/play2win-genesis"
-    className={cn(props.className)}
-  >
-    <Image
-      className="w-full rounded-xl"
-      alt="banner"
-      width={300}
-      height={100}
-      src="https://storage.onton.live/ontonimage/elympics_banner-1.png"
-    />
-  </Link>
-);
+export const Play2WinGenesisBanner: React.FC<React.SVGProps<SVGSVGElement>> = (props) => null;
+// <Link
+//   href="/play2win-genesis"
+//   className={cn(props.className)}
+// >
+//   <Image
+//     className="w-full rounded-xl"
+//     alt="banner"
+//     width={300}
+//     height={100}
+//     src="https://storage.onton.live/ontonimage/elympics_banner-1.png"
+//   />
+// </Link>
