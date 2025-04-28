fa4b0a905c95bce62d35dc87ce8b63d8a34cc22c
samyar <samyarkd@gmail.com>
2025-04-28 19:11:20 +0330

    fix: conflicts with branches

---
+++ mini-app/eslint.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactLint from "eslint-plugin-react";
+import hooksLint from "eslint-plugin-react-hooks";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  reactLint.configs.flat.recommended,
+  hooksLint.configs["recommended-latest"],
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+    },
+  }
+);
