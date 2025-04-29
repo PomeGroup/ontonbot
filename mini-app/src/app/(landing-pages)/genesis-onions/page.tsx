76067be06096c18057b2dbd4e39800a54d6ec4ca
samyar <samyarkd@gmail.com>
2025-04-29 10:18:08 +0330

    faet(onions): sbt cards

--- mini-app/src/app/(landing-pages)/genesis-onions/page.tsx
+++ mini-app/src/app/(landing-pages)/genesis-onions/page.tsx
@@ -3,18 +3,94 @@ import Typography from "@/components/Typography";
 import "./_assets/genesis-onions.css";
 import { Header } from "./_components/Header";
 import Image from "next/image";
+import { FaChevronRight } from "react-icons/fa6";
+import useWebApp from "@/hooks/useWebApp";
 
 export default function GenesisOnions() {
+  const webapp = useWebApp();
+
   return (
     <div>
       <Header />
       <main
-        className="bg-navy text-white min-h-screen p-4"
+        className="bg-navy text-white min-h-screen p-4 flex flex-col gap-5"
         style={{
           background: "radial-gradient(69.74% 28.27% at 50% 33.25%, #31517B 0%, #0A1C33 100%)",
         }}
       >
-        {/* Main Card */}
+        <div className="flex gap-4">
+          <button
+            onClick={() => {
+              webapp?.openLink(
+                "https://getgems.io/genesisonions?filter=%7B%22attributes%22%3A%7B%22color%22%3A%5B%22Gold%22%5D%7D%7D"
+              );
+            }}
+            className="border border-white p-2 flex flex-col gap-0.5 justify-center items-center bg-white/10 rounded-md backdrop-blur-lg w-full"
+          >
+            <Image
+              width={40}
+              height={40}
+              src="https://storage.onton.live/ontonimage/on_gold.jpg"
+              alt="NFT"
+              className="rounded-md aspect-square"
+            />
+            <Typography
+              variant="body"
+              weight="medium"
+              className="flex items-center gap-2 justify-center"
+            >
+              Gold <FaChevronRight size={12} />
+            </Typography>
+          </button>
+          <button
+            onClick={() => {
+              webapp?.openLink(
+                "https://getgems.io/genesisonions?filter=%7B%22attributes%22%3A%7B%22color%22%3A%5B%22Silver%22%5D%7D%7D"
+              );
+            }}
+            className="border border-white p-2 flex flex-col gap-0.5 justify-center items-center bg-white/10 rounded-md backdrop-blur-lg w-full"
+          >
+            <Image
+              width={40}
+              height={40}
+              src="https://storage.onton.live/ontonimage/on_silver.jpg"
+              alt="NFT"
+              className="rounded-md aspect-square"
+            />
+            <Typography
+              variant="body"
+              weight="medium"
+              className="flex items-center gap-2 justify-center"
+            >
+              Silver <FaChevronRight size={12} />
+            </Typography>
+          </button>
+          <button
+            onClick={() => {
+              webapp?.openLink(
+                "https://getgems.io/genesisonions?filter=%7B%22attributes%22%3A%7B%22color%22%3A%5B%22Bronze%22%5D%7D%7D"
+              );
+            }}
+            className="border border-white p-2 flex flex-col gap-0.5 justify-center items-center bg-white/10 rounded-md backdrop-blur-lg w-full"
+          >
+            <Image
+              width={40}
+              height={40}
+              src="https://storage.onton.live/ontonimage/on_bronze.jpg"
+              alt="NFT"
+              className="rounded-md aspect-square"
+            />
+            <Typography
+              variant="body"
+              weight="medium"
+              className="flex items-center gap-2 justify-center"
+            >
+              Bronze <FaChevronRight size={12} />
+            </Typography>
+          </button>
+        </div>
+        <div className="p-4 backdrop-blur-lg bg-white/10 rounded-md flex-col flex gap-3 items-center justify-center"></div>
+        {/* Description Card */}
         <div className="p-4 backdrop-blur-lg bg-white/10 rounded-md flex-col flex gap-3 items-center justify-center">
           <div className="p-4 backdrop-blur-md bg-black/20 rounded-md gap-5 flex flex-col">
             <div className="flex flex-col gap-2">
