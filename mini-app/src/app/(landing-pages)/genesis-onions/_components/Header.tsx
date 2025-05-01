af7fab7d6a4a6fe96de08fad14b3d21bf12f1a35
samyar <samyarkd@gmail.com>
2025-05-01 21:09:27 +0330

    merge banner

--- mini-app/src/app/(landing-pages)/genesis-onions/_components/Header.tsx
+++ mini-app/src/app/(landing-pages)/genesis-onions/_components/Header.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import GenesisOnionHead from "./../_assets/images/onion-genesis-merge-haed.svg";
 import { ONIONConnectWallet } from "./ONIONConnectWallet";
 import { formatPadNumber } from "@/lib/utils";
+import { getTimeLeft } from "@/lib/time.utils";
 
 export const Header = () => {
   const config = useConfig();
@@ -21,16 +22,7 @@ export const Header = () => {
     const endDate = endDateString ? new Date(endDateString) : null;
     if (!endDate) return;
     const updateTimer = () => {
-      const now = Date.now();
-      const distance = endDate.getTime() - now;
-      if (distance < 0) {
-        setTimeLeft({ days: 0, hours: 0, minutes: 0, seconds: 0 });
-        return;
-      }
-      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      const { days, hours, minutes, seconds } = getTimeLeft(endDate);
       setTimeLeft({ days, hours, minutes, seconds });
     };
     updateTimer();
--- mini-app/src/components/ui/button.tsx
+++ mini-app/src/components/ui/button.tsx
@@ -21,6 +21,8 @@ const buttonVariants = cva(
         link: "text-cn-primary underline-offset-4 text-zinc hover:underline",
         "primary-onion":
           "bg-onion text-white focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-opacity-50 disabled:opacity-100 disabled:bg-[#E8E8E9] disabled:text-[#6D6D72] hover:bg-orange",
+        "outline-onion":
+          "border border-onion-extraLight text-onion-extraLight hover:bg-onion/10 bg-white/5 backdrop-blur-md rounded-lg",
         "secondary-onion": "bg-white/20 text-white",
       },
       size: {
