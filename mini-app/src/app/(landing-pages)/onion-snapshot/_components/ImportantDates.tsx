0bc6160bd2674a9c7b25337839956255b6651625
samyar <samyarkd@gmail.com>
2025-05-21 12:22:21 +0330

    fix: things

--- mini-app/src/app/(landing-pages)/onion-snapshot/_components/ImportantDates.tsx
+++ mini-app/src/app/(landing-pages)/onion-snapshot/_components/ImportantDates.tsx
@@ -44,33 +44,29 @@ const ImportantDates = () => {
       <DateCard
         iconSrc="https://storage.onton.live/ontonimage/airdrop-calendar.svg"
         title="Token Generation Event"
+        link="https://onion.tg/docs/"
         date="June 20, 2025"
       />
 
       <DateCard
         iconSrc="https://storage.onton.live/ontonimage/airdrop-checklist.svg"
         title="DEX Listing Date"
+        link="https://onion.tg/docs/"
         date="June 20, 2025"
       />
 
       <DateCard
         iconSrc="https://storage.onton.live/ontonimage/airdrop-lock-open.svg"
         title="Genesis Airdrop Unlock"
+        link="https://onion.tg/docs/"
         date="June 20, 2025"
       />
 
       <DateCard
         iconSrc="https://storage.onton.live/ontonimage/airdrop-mountain.svg"
         title="Genesis Airdrop Cliff"
+        link="https://onion.tg/docs/"
         date="September 20, 2025"
-      />
-
-      {/* DAO Voting Begins Card */}
-      <DateCard
-        iconSrc="/images/voting-icon.svg"
-        title="DAO Voting Begins"
-        endDateKey="snapshot_voting_date"
-        description="Start voting using your ONIONs in ONIONVerse DAO to be a part of decisions."
       />
     </div>
   );
