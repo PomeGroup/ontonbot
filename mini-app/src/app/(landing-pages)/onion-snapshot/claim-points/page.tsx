dbc4e9560b0986fc684d379de3bb43fa025a6049
samyar <samyarkd@gmail.com>
2025-06-11 16:04:35 +0330

    fix: connect new wallet

--- mini-app/src/app/(landing-pages)/onion-snapshot/claim-points/page.tsx
+++ mini-app/src/app/(landing-pages)/onion-snapshot/claim-points/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { AlertGeneric } from "@/components/ui/alert";
 import { useTonWallet } from "@tonconnect/ui-react";
 import { ConnectNewWalletCard, OnionStockBanner, SnapshotResultCard } from "./_components";
 import { OnionFollowUsCard } from "./_components/OnionFollowUsCard";
@@ -10,6 +11,10 @@ export default function ClaimPointsPage() {
     <div className="flex flex-col gap-4 py-6 px-4">
       <OnionStockBanner />
       <SnapshotResultCard />
+      <AlertGeneric variant="info-light">
+        Seeing a “missing proof” error when connecting your wallet? Make one small transaction (even 0 TON, to any address or
+        to yourself) to activate the wallet, then try again.
+      </AlertGeneric>
       {wallet?.account.address ? <OnionFollowUsCard /> : <ConnectNewWalletCard buttonText="Connect Wallet" />}
     </div>
   );
