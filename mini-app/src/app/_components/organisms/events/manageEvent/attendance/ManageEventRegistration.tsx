2b639d3b159dbc4ddba9ac3156e19a126aea3f75
Samyar <samyarkd@gmail.com>
2025-07-04 16:47:38 +0330

    feat: registration

--- mini-app/src/app/_components/organisms/events/manageEvent/attendance/ManageEventRegistration.tsx
+++ mini-app/src/app/_components/organisms/events/manageEvent/attendance/ManageEventRegistration.tsx
@@ -2,10 +2,15 @@ import Typography from "@/components/Typography";
 import { AlertGeneric } from "@/components/ui/alert";
 import { Input } from "@/components/ui/input";
 import { Switch } from "@/components/ui/switch";
+import { useCreateEventStore } from "@/zustand/createEventStore";
 import ManageEventCard from "../ManageEventCard";
 import ManageEventDivider from "../ManageEventDivider";
 
 const ManageEventRegistration = () => {
+  const eventData = useCreateEventStore((state) => state.eventData);
+  const errors = useCreateEventStore((state) => state.rewardStepErrors);
+  const setEventData = useCreateEventStore((state) => state.setEventData);
+
   return (
     <ManageEventCard title="Registration">
       {/* Registration Form */}
@@ -17,7 +22,14 @@ const ManageEventRegistration = () => {
           >
             Registration Form
           </Typography>
-          <Switch />
+          <Switch
+            checked={eventData.has_registration}
+            onCheckedChange={(checked) => {
+              setEventData({
+                has_registration: checked,
+              });
+            }}
+          />
         </div>
         <div className="mt-1 flex-1 font-normal text-[13px] leading-4 tracking-normal text-[#3C3C4399]">
           Participants need to fill out a form with their information when joining your event.
@@ -26,46 +38,65 @@ const ManageEventRegistration = () => {
       {/* Divider */}
       <ManageEventDivider />
 
-      {/* Has Approval */}
-      <div>
-        <div className="flex justify-between items-center">
-          <Typography
-            variant="body"
-            weight="medium"
-          >
-            Approval Required
-          </Typography>
-          <Switch />
-        </div>
-        <div className="mt-1 flex-1 font-normal text-[13px] leading-4 tracking-normal text-[#3C3C4399]">
-          Your participants need to be approved by you after filling out the registration form.
-        </div>
-      </div>
-      {/* Divider */}
-      <ManageEventDivider />
-      {/* Has Approval */}
-      <div>
-        <div className="flex justify-between items-center">
-          <Typography
-            variant="body"
-            weight="medium"
-          >
-            Capacity
-          </Typography>
-          <Switch />
-        </div>
-        <div className="mt-1 flex-1 font-normal text-[13px] leading-4 tracking-normal text-[#3C3C4399]">
-          Max Capacity is 100 guests. Automatically closes registration once capacity is reached, Counting only approved
-          guests.
-        </div>
-        <Input
-          label="Max Capacity"
-          placeholder="100"
-          name="capacity"
-        />
-      </div>
-      {/* Divider */}
-      <ManageEventDivider />
+      {eventData.has_registration && (
+        <>
+          {/* Has Approval */}
+          <div>
+            <div className="flex justify-between items-center">
+              <Typography
+                variant="body"
+                weight="medium"
+              >
+                Approval Required
+              </Typography>
+              <Switch
+                checked={eventData.has_approval}
+                onCheckedChange={(checked) => {
+                  setEventData({
+                    has_approval: checked,
+                  });
+                }}
+              />
+            </div>
+            <div className="mt-1 flex-1 font-normal text-[13px] leading-4 tracking-normal text-[#3C3C4399]">
+              Your participants need to be approved by you after filling out the registration form.
+            </div>
+          </div>
+          {/* Divider */}
+          <ManageEventDivider />
+          {/* Has Approval */}
+          <div>
+            <div className="flex justify-between items-center">
+              <Typography
+                variant="body"
+                weight="medium"
+              >
+                Capacity
+              </Typography>
+            </div>
+            <div className="mt-1 flex-1 font-normal text-[13px] leading-4 tracking-normal text-[#3C3C4399]">
+              Max Capacity is 100 guests. Automatically closes registration once capacity is reached, Counting only approved
+              guests.
+            </div>
+            <Input
+              label="Max Capacity"
+              placeholder="100"
+              name="capacity"
+              type="number"
+              min={1}
+              defaultValue={eventData.capacity || 100}
+              onBlur={(e) => {
+                e.preventDefault();
+                setEventData({
+                  capacity: e.target.value ? parseInt(e.target.value, 10) : 100,
+                });
+              }}
+            />
+          </div>
+          {/* Divider */}
+          <ManageEventDivider />
+        </>
+      )}
 
       {/* Proof of attendance */}
       <div className="flex flex-col gap-3">
@@ -85,7 +116,16 @@ const ManageEventRegistration = () => {
         <Input
           label="Event Password"
           placeholder="Your Event’s Passkey"
+          errors={errors?.secret_phrase}
           name="event_password"
+          defaultValue={eventData.secret_phrase || ""}
+          onBlur={(e) => {
+            e.preventDefault();
+            if (Boolean(e?.target?.value?.trim()))
+              setEventData({
+                secret_phrase: (e.target.value as string).toLowerCase().trim(),
+              });
+          }}
         />
         <AlertGeneric variant="info-light">Password is case-insensitive and must be at least 4 characters.</AlertGeneric>
       </div>
